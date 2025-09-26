00d16d6c5c66b1e581f844c726997a969c246282
Celia Parts <cparts@stsci.edu>
2025-09-26 13:56:18 -0400

    adding get_overlays call to each add overlays method

--- src/ipyaladin/widget.py
+++ src/ipyaladin/widget.py
@@ -597,6 +597,7 @@ class Aladin(anywidget.AnyWidget):
                 "options": catalog_options,
             }
         )
+        self.get_overlays()
 
     def _save_file(self, path: str, buffer: bytes) -> None:
         """Save a file from a buffer.
@@ -720,6 +721,7 @@ class Aladin(anywidget.AnyWidget):
                 "options": votable_options,
             }
         )
+        self.get_overlays()
 
     @widget_should_be_loaded
     def add_fits(self, fits: Union[str, Path, HDUList], **image_options: any) -> None:
@@ -925,6 +927,7 @@ class Aladin(anywidget.AnyWidget):
             {"event_name": "add_table", "options": table_options},
             buffers=[table_bytes.getvalue()],
         )
+        self.get_overlays()
 
     @widget_should_be_loaded
     def add_graphic_overlay_from_region(
@@ -1005,6 +1008,7 @@ class Aladin(anywidget.AnyWidget):
                 "graphic_options": graphic_options,
             }
         )
+        self.get_overlays()
 
     @widget_should_be_loaded
     def add_overlay_from_stcs(
@@ -1070,6 +1074,7 @@ class Aladin(anywidget.AnyWidget):
                 "graphic_options": overlay_options,
             }
         )
+        self.get_overlays()
 
     @widget_should_be_loaded
     def remove_overlay(self, overlay_name: Union[Iterable[str], str]) -> None:
